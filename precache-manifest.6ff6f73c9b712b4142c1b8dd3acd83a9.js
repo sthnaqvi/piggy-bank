b52265424be2dcae33a7d85dffc4927506be44bb
Tauseef Naqvi <tauseefnaqvi@Tauseefs-MacBook-Air.local>
2018-10-07 23:09:01 +0530

    first commit

---
+++ precache-manifest.6ff6f73c9b712b4142c1b8dd3acd83a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4883816e4c99a3cc26ac",
+    "url": "/static/js/main.4883816e.chunk.js"
+  },
+  {
+    "revision": "474e25a3d1823dab89e7",
+    "url": "/static/js/1.474e25a3.chunk.js"
+  },
+  {
+    "revision": "4883816e4c99a3cc26ac",
+    "url": "/static/css/main.81bae47e.chunk.css"
+  },
+  {
+    "revision": "d8306c3079a6b9ce358ebeb89ceb3713",
+    "url": "/index.html"
+  }
+];

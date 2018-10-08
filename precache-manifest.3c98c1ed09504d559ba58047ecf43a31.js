24302bea6b796fd677cafaa98e55ab306987fde4
Sayed Tauseef Naqvi <tauseef.naqvi786@gmail.com>
2018-10-08 22:47:02 +0530

    fix design

---
+++ precache-manifest.3c98c1ed09504d559ba58047ecf43a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91aed278d5f754b51fce",
+    "url": "/static/js/main.91aed278.chunk.js"
+  },
+  {
+    "revision": "f3c2c3310c085069e8c7",
+    "url": "/static/js/1.f3c2c331.chunk.js"
+  },
+  {
+    "revision": "91aed278d5f754b51fce",
+    "url": "/static/css/main.076142ec.chunk.css"
+  },
+  {
+    "revision": "7b95d6df850c60d7d417d936df0c8518",
+    "url": "/index.html"
+  }
+];
